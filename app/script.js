cefd194e11ad07caac648c994ab0c2ef4f5fd4db
Jason Studstill <jason@jasonstudstill.com>
2015-10-08 14:24:48 -0700

    Riley's submission

---
+++ app/script.js
@@ -0,0 +1,5 @@
+$.get("http://localhost:3000/myJson", function (response) {
+
+ $("#jsonhere").text(response);}
+
+);
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var bodyParser = require('body-parser')
 
-app.get('/jsonobj', function(req, res) {
-  res.send({"breed":"boxer"});
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/app/'));
+
+var myJsonObj =
+    {"dinner":"so good", "numberOfDinners":"44"}
+
+app.get("/myJson",function(req, res){
+ res.json(myJsonObj);
 });
 
 app.listen(PORT, function() {
